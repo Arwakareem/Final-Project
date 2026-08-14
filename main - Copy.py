64f86f47db3b724931063f28a177c527a2605899
ArwaKareem <arwakareem2020@gmail.com>
2026-08-14 09:59:08 -0700

    Initial commit: project setup

---
+++ main - Copy.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv("C:\Users\almnara\Desktop\Final Project\book_fair_collected.csv")
+print(df.head())
--- main.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv("C:\Users\almnara\Desktop\Final Project\book_fair_collected.csv")
-print(df.head())
